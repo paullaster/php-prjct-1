a301a8a58658f99a7e975b7141428b0cdc2974d7
paullaster <paullasterokoth98@gmail.com>
2023-03-20 13:30:44 +0300

    php files

--- laracast/homework-2.php
+++ laracast/homework-2.php
@@ -10,7 +10,7 @@
             function filterMovies($moviesArray, $releaseYear) {
                 $filteredMovies = [];
                 foreach($moviesArray as $movie){
-                    if($movie['releaseYear'] >= 2000){
+                    if($movie['releaseYear'] >= $releaseYear){
                         $filteredMovies[] = $movie;
                     }
                 return $filteredMovies;    
@@ -82,7 +82,7 @@
          ];
         ?>
         <ul>
-            <?php foreach ($movies as $movie) : ?>
+            <?php foreach (filterMovies($movies, 2000) as $movie) : ?>
                 <li>
                     <span>movie title: </span><?=$movie['title']?> <br/>
                     <span>movie category: </span><?=$movie['category']?> <br/>
