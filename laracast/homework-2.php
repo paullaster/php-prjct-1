3c9e566a555dfb10b7b1e1b9f56900bcbaf7fc87
paullaster <paullasterokoth98@gmail.com>
2023-03-20 12:46:45 +0300

    php files

--- laracast/homework-2.php
+++ laracast/homework-2.php
@@ -22,7 +22,52 @@
                     ]
                     ],
                     'releaseYear' => 2019
-            ]
+                ],
+                [
+                    'title' => 'Maria',
+                    'category' => 'Drama and Romance',
+                    'mainCharacters' =>[
+                        [
+                            'name' => 'Maria Hamisa\'',
+                            'age' => 25,
+                        ],
+                        [
+                            'name' => 'Lui Clif',
+                            'age' => 26,
+                        ]
+                        ],
+                        'releaseYear' => 2021
+                    ],
+                    [
+                        'title' => 'Tom and Jerry',
+                        'category' => 'Thriller',
+                        'mainCharacters' =>[
+                            [
+                                'name' => 'Jerry Hueng',
+                                'age' => 32,
+                            ],
+                            [
+                                'name' => 'Tom Daniela',
+                                'age' => 36,
+                            ]
+                            ],
+                            'releaseYear' => 1995
+                        ],
+                        [
+                            'title' => 'Masterred',
+                            'category' => 'Science Finction',
+                            'mainCharacters' =>[
+                                [
+                                    'name' => 'Luanga Lowered',
+                                    'age' => 42,
+                                ],
+                                [
+                                    'name' => 'morgan Heritage',
+                                    'age' => 46,
+                                ]
+                                ],
+                                'releaseYear' => 1990
+                        ]
          ];
         ?>
     </body>
