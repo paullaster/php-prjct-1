f08ad89b681c2243c53451ccbd7eb5177f4ad149
paullaster <paullasterokoth98@gmail.com>
2023-03-20 14:05:12 +0300

    php files

--- laracast/homework-3.php
+++ laracast/homework-3.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
+        <title>PHP LAMBDA FUNCTIONS : ANONYMOUS FUNCTION</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
